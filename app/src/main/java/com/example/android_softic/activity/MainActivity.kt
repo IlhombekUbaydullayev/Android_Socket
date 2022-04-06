5f6e1fccc927227f3c89de528914bf907e11d128
IlhombekUbaydullayev <ubaydullaevilhombek682@gmail.com>
2022-04-06 21:04:47 +0500

    socket

--- app/src/main/java/com/example/android_softic/activity/MainActivity.kt
+++ app/src/main/java/com/example/android_softic/activity/MainActivity.kt
@@ -1,39 +1,78 @@
 package com.example.android_softic.activity
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android_softic.model.BitCoin
 import com.example.android_softic.R
 import com.example.android_softic.manager.SocketListener
 import com.example.android_softic.manager.WebSocketBtc
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import okhttp3.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
 
-     lateinit var tv_socket: TextView
      private lateinit var webSocketBtc: WebSocketBtc
+     private var lineValues = ArrayList<Entry>()
+    private var count = 0
+    lateinit var line_chart : LineChart
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
+        configureLineChart()
+    }
+
+    private fun configureLineChart() {
+        val desc = Description()
+        desc.text = "BTC-USD"
+        desc.textSize = 20F
+
+
+        line_chart.description = desc
+        val xAxis: XAxis = line_chart.xAxis
+        xAxis.valueFormatter = object : ValueFormatter() {
+            @RequiresApi(Build.VERSION_CODES.N)
+            private val mFormat: SimpleDateFormat = SimpleDateFormat("HH mm", Locale.getDefault())
+
+            @RequiresApi(Build.VERSION_CODES.N)
+            override fun getFormattedValue(value: Float): String {
+                return mFormat.format(Date(System.currentTimeMillis()))
+            }
+        }
     }
 
     private fun initViews() {
+        line_chart = findViewById(R.id.line_chart)
         webSocketBtc = WebSocketBtc()
-        tv_socket = findViewById(R.id.tv_socket)
-
         webSocketBtc.connectToSocket("live_trades_btcusd")
         webSocketBtc.socketListener(object : SocketListener {
             override fun onSuccess(bitCoin: BitCoin) {
+                count++
                 runOnUiThread {
                     if (bitCoin.event == "bts:subscription_succeeded") {
                         Toast.makeText(this@MainActivity,"Successfull Connected",Toast.LENGTH_SHORT).show()
                     }else {
-                        tv_socket.text = bitCoin.data.amount.toString()
+                        lineValues.add(Entry(count.toFloat(),bitCoin.data.price.toFloat()))
+                        setLineChartData(lineValues)
                     }
                 }
             }
@@ -43,5 +82,24 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
+
+    }
+
+    private fun setLineChartData(pricesHigh: ArrayList<Entry>) {
+        val dataSets: ArrayList<ILineDataSet> = ArrayList()
+        val highLineDataSet = LineDataSet(pricesHigh,"Live")
+        highLineDataSet.setDrawCircles(true)
+        highLineDataSet.circleRadius = 4f
+        highLineDataSet.setDrawValues(false)
+        highLineDataSet.lineWidth = 3f
+        highLineDataSet.color = Color.GREEN
+        highLineDataSet.setCircleColor(Color.YELLOW)
+        dataSets.add(highLineDataSet)
+
+        val lineData = LineData(dataSets)
+        line_chart.data = lineData
+        line_chart.invalidate()
+
     }
 }
--- app/src/main/java/com/example/android_softic/model/Data.kt
+++ app/src/main/java/com/example/android_softic/model/Data.kt
@@ -2,6 +2,7 @@ package com.example.android_softic.model
 
 data class Data (
     val amount: Double,
+    val price: Double,
     val amount_str: String,
     val buy_order_id: Long,
     val id: Int,
